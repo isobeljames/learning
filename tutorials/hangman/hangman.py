331c05c20f857b9aae1bbf2ca5a1b5d2ceb37fcc
Isobeljames <isobeljjames@gmail.com>
2021-09-11 09:59:49 +0200

    hm with functions

--- tutorials/hangman/hangman.py
+++ tutorials/hangman/hangman.py
@@ -1,4 +1,4 @@
-import random
+import random, hm_images, os
 from hm_words import words
 
 word = random.choice(words)
@@ -6,24 +6,40 @@ reveal = list(len(word)*'_')
 lives = 7
 gameWon = False
 
+def check_letter(letter, word):
+    global reveal # using the global reveal as this is what you're changing
+    for i in range(0,len(word)):
+        letter = word[i]
+        if guess == letter:
+            reveal[i] = guess
+    if '_' not in reveal:
+        return True
+    else:
+        return False
+
+def status():
+    os.system('clear')
+    print(hm_images.hangman[7-lives]) # which image to access
+    print("\n")
+    print(' '.join([str(e) for e in reveal]))
+    print("You have", lives, "lives remaining")
+    print("\n")
+
 while gameWon == False and lives > 0:
-    print(reveal)
+    status()
     guess = input("Guess a letter or an entire word:")
     guess = guess.upper()
 
     if guess == word:
         gameWon = True
+        reveal = word
     if len(guess) == 1 and guess in word:
-        for i in range(0,len(word)):
-            letter = word[i]
-            if guess == letter:
-                reveal[i] = guess
-        if '_' not in reveal:
-            gameWon = True
+        gameWon = check_letter(guess, word)
     else:
         lives -= 1
+    status() # so it prints out the word at the end
 
 if gameWon:
-    print("Well done, you guessed it!")
+    print("WELL DONE, YOU GUESSED IT!")
 else:
-    print(f"You failed, the word was {word}")
+    print(f"YOU FAILED, THE WORD WAS {word}")
